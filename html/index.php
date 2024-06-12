28921185a3c1447ab93ac647efd1ff6fbff35dce
蔡銘凱 <z85385637@gmail.com>
2024-06-12 21:23:16 +0800

    test

--- html/index.php
+++ html/index.php
@@ -39,7 +39,8 @@
         <p>You have successfully deployed a <strong>docker</strong> container running our <strong>NGINX</strong> with <strong>PHP-FPM 8.2.x</strong> image</p>
         <p><strong>NGINX: </strong>v<?php echo $_ENV['NGINX_VERSION'] ?><br><strong>PHP-FPM: </strong>v<?php echo phpversion(); ?><br><strong>LOADED CONFIG: </strong><?php echo php_ini_loaded_file(); ?><br><strong>WEB ROOT: </strong><?php echo $_ENV['DOCUMENT_ROOT'] ?><br><strong>HOSTNAME: </strong><?php echo gethostname(); ?><br></p>
           <em>Thank you for using <a style="text-decoration:none" href="https://wyveo.com" target="_blank">wyveo.com</a></em>
-        <p><?php getenv('MY_VARIABLE');?></p>
+        <p><?php echo  $_ENV['MY_VARIABLE'];?></p>
+        <p><?php echo  $_ENV['test'];?></p>
       </div>
     </div>
   </div>
